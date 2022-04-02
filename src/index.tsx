f812d3ff6a24e8a65bab220ca5ca894843627531
Serjge <serjge@yandex.ru>
2022-04-02 11:44:25 +0300

    added gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
-import './index.css';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import * as serviceWorker from './serviceWorker';
 
 import { App } from 'App';
 import { store } from 'store/store';
+import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       {' '}
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root'),
 );
 
